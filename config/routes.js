f1f17f3a6248dd47eef37d4d40f8d1d838ff6f38
SilverFlame83 <imren_83@abv.bg>
2021-06-20 18:11:44 +0300

    Home template created

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const authController  = require('../controllers/authController');
-
+const homeController = require('../controllers/homeController');
 
 module.exports = (app) =>{
-    app.use('/auth', authController)
+    app.use('/', homeController);
+    app.use('/auth', authController);
 }
